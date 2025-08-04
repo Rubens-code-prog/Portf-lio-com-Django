e9d97d53d34129462925f06bd3c73cb29eb417c6
Rubens Gabriel <rugabriel.gabriel@gmail.com>
2025-08-04 17:11:56 -0300

    Portfolio de projetos

---
+++ portfolio/dados.py
@@ -0,0 +1,95 @@
+habilidades = {
+    'habilidade1': {
+        'nome': 'Análise de dados',
+    },
+    'habilidade2': {
+        'nome': 'Automação de processos',
+    },
+    'habilidade3': {
+        'nome': 'Python',
+    },
+    'habilidade4': {
+        'nome': 'Power BI',
+    },
+    'habilidade5': {
+        'nome': 'Excel',
+    },
+    'habilidade6': {
+        'nome': 'Valuation',
+    },
+    'habilidade7': {
+        'nome': 'Contabilidade',
+    },
+    'habilidade8': {
+        'nome': 'Django',
+    },
+    'habilidade9': {
+        'nome': 'Git e GitHub',
+    },
+    'habilidade10': {
+        'nome': 'HTM',
+    },
+}
+
+projetos = {
+    'projeto1': {
+        'título': 'ETFs Wiki', 
+        'descricao': 'O primeiro projeto que desenvolvi',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto2': {
+        'título': 'Automação de processos', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto3': {
+        'título': 'Python', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto4': {
+        'título': 'Power BI', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto5': {
+        'título': 'Excel', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto6': {
+        'título': 'Valuation', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto7': {
+        'título': 'Contabilidade', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto8': {
+        'título': 'Django', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto9': {
+        'título': 'Git e GitHub', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+    'projeto10': {
+        'título': 'HTM', 
+        'descricao': 'O primeiro projeto',
+        'tecnologia': 'Python e APIs',
+        'Repositório': 'Link'
+        },
+}
